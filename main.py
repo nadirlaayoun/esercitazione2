e45fad3b21e856777664250ff123ce2cf52ae5c3
nadir <laayounnadir@gmail.com>
2020-03-23 11:52:47 +0100

    aggiunte 5 righe

---
+++ main.py
@@ -0,0 +1,12 @@
+x = 5
+for i in range(x):
+    x+=i
+print(x)
+print("ciao")
+
+nome = input("dimmi il tuo nome")
+
+if nome == "nadir":
+    print("fantasmagorico")
+else:
+    print("non fantasmagorico")
